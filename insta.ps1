b34bdf694c9cd6f37c2e1d8121c3b2d8286ea1a6
vici <cjh425@gmail.com>
2024-05-07 21:32:44 -0500

    add top post python script

--- insta.ps1
+++ insta.ps1
@@ -1,2 +1,2 @@
-instaloader - -post-filter = "not is_video and likes>10" mpalace2012
-
+instaloader --post-filter="not is_video and likes>10" mpalace2012
+instaloader --post-metadata-txt="{likes} likes." mpalace2012
